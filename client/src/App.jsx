50046446690ba9575ba0a65335b31300a924e8ad
Siddhant Gavai <siddhantgavai0@gmail.com>
2026-04-09 10:24:34 +0530

    refactor: Move ThemeProvider import to the top of App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 // Pages
 import Login from './pages/Login';
@@ -79,8 +80,6 @@ const AppRoutes = () => {
         </Routes>
     );
 };
-
-import { ThemeProvider } from './context/ThemeContext';
 
 const App = () => {
     return (

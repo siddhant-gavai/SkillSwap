c490889899ac5d2b034419179309f335e9b39470
Siddhant Gavai <siddhantgavai0@gmail.com>
2026-05-18 22:35:28 +0530

    Update Login link transition in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
                             </>
                         ) : (
                             <div className="flex items-center space-x-4">
-                                <Link to="/login" className="text-gray-600 hover:text-indigo-600">Login</Link>
+                                <Link to="/login" className="text-gray-600 hover:text-indigo-600 transition-colors">Login</Link>
                                 <Link to="/register" className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition">Get Started</Link>
                             </div>
                         )}

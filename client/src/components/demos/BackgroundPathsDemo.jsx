f3b6aea9f3a8bc371728317cd2b3a701a837bbba
Siddhant Gavai <siddhantgavai0@gmail.com>
2026-07-16 13:26:26 +0530

    Delete 21.dev component files and demo files

--- client/src/components/demos/BackgroundPathsDemo.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { BackgroundPaths } from "@/components/ui/BackgroundPaths";
-
-export const BackgroundPathsDemo = () => {
-    return <BackgroundPaths title="Background Paths" />;
-};
-
-export default BackgroundPathsDemo;
